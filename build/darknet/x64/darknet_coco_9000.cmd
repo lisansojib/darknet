b6789986f62e1e9a3ae7aeabd13a93e5c8a04436
Administrator <lisancseru@gmail.com>
2018-06-22 04:06:34 -0400

    command updated

--- build/darknet/x64/darknet_coco_9000.cmd
+++ build/darknet/x64/darknet_coco_9000.cmd
@@ -1,5 +1,5 @@
 
-darknet.exe detector test cfg/combine9k.data yolo9000.cfg yolo9000.weights data/dog.jpg
+darknet_no_gpu.exe detector test cfg/combine9k.data yolo9000.cfg yolo9000.weights data/dog.jpg
 
 
 pause
